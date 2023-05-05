8cabbfbab48bf33ce566628c570d47c14a2f1df6
breezjirasak <jirasak.tu@ku.th>
2023-05-06 00:03:22 +0700

    Implement Solider

--- frameworks/Soldier.java
+++ frameworks/Soldier.java
@@ -1,5 +1,53 @@
 package frameworks;
 
-public class Soldier{
-    
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public abstract class Soldier {
+    private int hp;
+    private int damage;
+    public String side;
+    public int positionX;
+    public int positionY;
+
+    public BufferedImage walk1, walk2;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+
+    public Soldier(int hp, int damage, int x, int y, String side) {
+        this.hp = hp;
+        this.damage = damage;
+        this.positionX = x;
+        this.positionY = y;
+        this.side = side;
+
+    }
+
+    public void getImage(String weapon) {
+        try {
+            walk1 = ImageIO.read(new File("images/soldier/" + weapon + "/" + side + "walk1.png"));
+            walk2 = ImageIO.read(new File("images/soldier/" + weapon + "/" + side + "walk2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takeDamage(int damage) {
+        hp -= damage;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public abstract void update();
+
+    public abstract void draw(Graphics g);
 }
--- objects/soldiers/SwordSoldier.java
+++ objects/soldiers/SwordSoldier.java
@@ -1,7 +1,50 @@
 package objects.soldiers;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import frameworks.Soldier;
 
 public class SwordSoldier extends Soldier {
-    
+
+    public SwordSoldier(int x, int y, String side) {
+        super(100, 40, x, y, side);
+
+        getImage("swordsolider");
+    }
+
+    @Override
+    public void update() {
+        if (side == "green") {
+            positionX -= 1;
+        } else if (side == "red") {
+            positionX += 1;
+        }
+
+        spriteCounter++;
+        if (spriteCounter > 15) {
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        BufferedImage image = null;
+
+        if (spriteNum == 1) {
+            image = walk1;
+        }
+        if (spriteNum == 2) {
+            image = walk2;
+        }
+
+        g.drawImage(image, positionX, positionY, 120, 120, null);
+
+    }
+
 }
